07fe4f15c840791aac55e48c0859b897173efebb
jeanlyn <jeanlyn92@gmail.com>
2014-03-15 16:59:05 +0800

    add the hosts page

---
+++ webapp/clurterinit.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ -d tmp ] ; then
+    exit 0
+fi
+mkdir -p tmp
+touch tmp/hosts
+touch tmp/manage
--- webapp/main.py
+++ webapp/main.py
@@ -5,12 +5,25 @@ import os
 import uimodules
 import sys
 from tool.hconf import HadoopConf
+from tool.tool import *
 import json
+import logging
+#日志配置
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',datefmt='%a, %d %b %Y %H:%M:%S',filename='web.log',filemode='w')
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         item = ["item1","item2","item3"]
         self.render("main.html",title="集市自动化管理",item=item)
+
+    def post(self):
+        if runshcommand("sh clurterinit.sh") == None:   
+            self.set_header("Content-Type","application/text")
+            self.write("error")     
+        else:
+            self.set_header("Content-Type","application/text")
+            self.write("success")
+            #redirect
 
 class configureHandler(tornado.web.RequestHandler):
     """the configure html"""
@@ -37,12 +50,61 @@ class configuresHandler(tornado.web.RequestHandler):
             self.set_status(201)
             self.write("保存成功!")
         except Exception, e:
+            logging.error(e)
             self.set_header("Content-Type","application/text")
             self.set_status(201)
-            self.write("出现错误:"+e)
-                
- 
-   
+            self.write(e)
+#step 2: add host                
+class hostsHandler(tornado.web.RequestHandler):
+    def get(self):
+        try:
+            self.render("hosts.html",title="添加hosts")
+        except Exception, e:
+            logging.error(e)
+        
+    def post(self):
+        pass
+
+#step 2:load data by ajax
+class hostsloadHandler(tornado.web.RequestHandler):
+    """
+       handle the data by ajax
+    """
+    def get(self,paramer):
+        try:
+            hosts=readhosts()
+            self.set_header("Content-Type","application/json")
+            self.write(json.dumps(hosts))
+
+        except Exception, e:
+            print(e)
+            logging.error(e)
+            self.set_header("Content-Type","application/text")
+            self.write("error")
+
+    def post(self,paramer):
+        action=paramer.split('?')[0]
+        path='tmp/hosts'
+        try:
+            if action == 'save':
+                data=json.loads(self.request.body)
+                string='\n'.join(['\t'.join(x.values()) for x in data])
+                with open(path,'w') as f:
+                    f.write(string)
+                self.set_header('Content-Type','application/text')
+                self.write("success")
+            else :
+                if self.request.files.get('uploadfile',None):
+                    uploadfile=self.request.files['uploadfile'][0]
+                    with open(path,'w') as f:
+                        f.write(uploadfile['body'])
+                self.redirect('/hosts')
+        except Exception, e:
+            logging.error(e)
+            self.set_header('Content-Type','application/text')
+            self.write("error")        
+
+#step 3:add cluster username and password   
 
 #setting for application
 settings = {
@@ -55,7 +117,10 @@ settings = {
 
 #luyou yinshe
 application = tornado.web.Application([
-    (r"/", MainHandler),
+    (r"/",MainHandler),
+    (r"/\?.+",MainHandler),
+    (r"/hosts",hostsHandler),
+    (r"/hosts/(.+)",hostsloadHandler),
     (r"/configure",configureHandler),
     (r'/configure/(.+)',configuresHandler),
 ],**settings)
